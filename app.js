bdd34078b8eeaf8d3c4008d76df314f54a56a397
isha-03 <ijain2_be21@thapar.edu>
2023-06-18 18:31:39 +0530

    dance website

--- app.js
+++ app.js
@@ -14,9 +14,12 @@ app.set('views',path.join(__dirname,'views'));
 
 app.get("/",(req,res)=>{
     const params={}
-    res.status(200).render('index.pug',params);
+    res.status(200).render('home.pug',params);
 });
-
+app.get("/contact",(req,res)=>{
+    const params={}
+    res.status(200).render('contact.pug',params);
+});
 //start the server
 app.listen(port,()=>{
     console.log(`the application started successfully on port ${port}`);
